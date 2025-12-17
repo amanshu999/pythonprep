0d97d7cef6c44d7ec664a324fe0b035f53979c9e
Amanshu Sharma <amanshusharma404@gmail.com>
2025-12-17 19:26:53 +0530

    tryn

--- 1.py
+++ 1.py
@@ -1,2 +1,13 @@
 print ("Hello world")
 
+a = input("enter text: ")
+print(a)
+
+b = int(input("Enter number: "))
+c = b+10
+print(c)
+
+d = float(input("enter floating point number: "))
+e = d*10
+print(e)
+
